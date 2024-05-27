240db6a25cc26ddc2d7f9513eecfb9fbe73b75bd
Juanuth <142501793+Juanuth@users.noreply.github.com>
2024-05-27 15:53:18 -0300

    a

--- Assets/Login_Manager.cs
+++ Assets/Login_Manager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Login_Manager : MonoBehaviour
 {
+    public InputField Contraseña;
+    public string LaContra;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,17 @@ public class Login_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Escribir_Contraseña()
+    {
+        if (Contraseña.text == LaContra)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
